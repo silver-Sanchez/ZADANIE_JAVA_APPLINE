750cdb6f843812f71f2e876f0745c1245de2a1e4
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-04-19 19:37:46 +0300

    Задание №9: Ввод/вывод данных для массива Произведите ввод данных с клавиатуры в массив, а после этого произведите вывод массива на экран, где каждый элемент массива умножается на 2. Размер массива задается пользователем.

--- src/com/company/massiv.java
+++ src/com/company/massiv.java
@@ -9,13 +9,16 @@ import java.util.ArrayList;
 public class massiv {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите размер массива");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println("Введите числа в массив равный " + a);
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < a; i++)
         {
             list.add(Integer.parseInt(reader.readLine()));
         }
-        for (int c = 0; c < 2; c++)
+        for (int c = 0; c < a; c++)
         {
             System.out.println(list.get(c) *2);
         }
