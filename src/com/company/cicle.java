cf41f53492feaecb3cfca817f8d7d60c482b30ad
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-04-19 18:44:37 +0300

    Initial commit

---
+++ src/com/company/cicle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class cicle {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 51; i++) {
+            System.out.println(i);
+        }
+    }
+}
