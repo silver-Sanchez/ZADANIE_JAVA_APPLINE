0dfee1cac8afd545eb7eddc7511971f39b533575
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-04-21 22:08:35 +0300

    Задача №4: Отработка поразрядных операций Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран. --исправил вывод---

--- src/com/company/binary.java
+++ src/com/company/binary.java
@@ -1,6 +1,9 @@
 package com.company;
 import java.util.Scanner;
 
+//        Задача №4: Отработка поразрядных операций
+//        Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+
 public class binary {
 
     public static void main(String[] args) {
