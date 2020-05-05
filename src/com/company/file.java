31f973466d4e31e1c9895ff6bd42fcc84c66162f
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-05-05 14:17:06 +0300

    Задача №16: Выгрузка данных из файла на экран Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt

---
+++ src/com/company/file.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+//Задача №16: Выгрузка данных из файла на экран
+//        Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt
+
+public class file {
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream("./text.txt"), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+}
