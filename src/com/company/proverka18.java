d4f314c07bf80f7dc4ed0b1f9586345dcd379682
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-05-08 13:29:02 +0300

    Задача №18: Выгрузка данных из файла, проверка и перезапись

--- src/com/company/proverka18.java
+++ src/com/company/proverka18.java
@@ -2,6 +2,11 @@ package com.company;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+//Задача №18: Выгрузка данных из файла, проверка и перезапись
+//Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
+//        а потом в этот же файл перезаписывать текстовые данные, введенные вручную. Количество
+//        строк после перезаписи должно быть столько же, сколько и в изначальном варианте.
+
 public class proverka18 {
     public static void main(String[] args) {
 
@@ -16,10 +21,12 @@ public class proverka18 {
                 lines++;
                 System.out.println(line);
             }
-            if (file.exists()) {
-                file.delete();
+          if (file.exists()) {
+               file.delete();
                 file.createNewFile();
-            }
+          }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,12 +37,15 @@ public class proverka18 {
         String str = null;
         try (FileWriter writer = new FileWriter(file, true)) {
             for (int i = 0; i < lines; i++) {
-                str = reader.readLine();;
+                System.out.println("Осталось заменить строк: " + (lines - i));
+                str = reader.readLine();
                 writer.write(str + "\n");
             }
+            System.out.println("Все строки были заменены");
             writer.flush();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
+
         }
     }
 }
